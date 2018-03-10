9a41e07c10f1e2f2509434caa3570d1926e4e6e2
kaikalista <edelcastillo82@gmail.com>
2018-03-10 22:44:12 +0100

    changes in portfolio template

--- wp-content/themes/allegiant/template-parts/element-page-header.php
+++ wp-content/themes/allegiant/template-parts/element-page-header.php
@@ -6,10 +6,8 @@
 <?php if($header_image != '') $header_image = 'style="background-image:url('.esc_url($header_image).');"'; ?>
 <?php do_action('cpotheme_before_title'); ?>
 <section id="pagetitle" class="pagetitle dark" <?php echo $header_image; ?>>
-	<div class="container">
 		<?php do_action('cpotheme_title'); ?>
 		<?php cpotheme_post_media( get_the_ID(), 'image' ); ?>
-	</div>
 </section>
 <?php do_action('cpotheme_after_title'); ?>
 
